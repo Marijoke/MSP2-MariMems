40173374cb7207782424c1ec49116f6de4c7c672
Marijoke <mariam.adamu@hotmail.co.uk>
2021-03-13 14:47:10 +0000

    fixed game crashing when multiple cards turn over before original returns by using lockBoard to only allow two cards to be turned at once

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard(){
+    if (lockBoard) return;
     this.classList.add('flip');
     if (!hasFlippedCard) {
         hasFlippedCard = true;
@@ -28,9 +30,11 @@ function disableCards() {
 }
 
 function unflipCards(){
+    lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        lockBoard = false;
     }, 1500);
 }
 
