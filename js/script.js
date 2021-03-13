948c75f780ebed655150bb2250c3416ac41c869f
Marijoke <mariam.adamu@hotmail.co.uk>
2021-03-13 16:29:19 +0000

    included a reset for the cards to stop double clicking crashing the game.

--- js/script.js
+++ js/script.js
@@ -6,36 +6,37 @@ let firstCard, secondCard;
 
 function flipCard(){
     if (lockBoard) return;
+    if (this === firstCard) return;
     this.classList.add('flip');
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
-        return
+        return;
     } 
-        hasFlippedCard = false;
         secondCard = this;
         
        checkForMatch();
     }
-
 function checkForMatch() {
     let isMatch = firstCard.dataset.name === secondCard.dataset.name;
      
     isMatch ? disableCards() : unflipCards();
 }
-
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    resetBoard();
 }
-
 function unflipCards(){
     lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
-        lockBoard = false;
+        resetBoard();
     }, 1500);
 }
-
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
 cards.forEach(card => card.addEventListener('click', flipCard));
