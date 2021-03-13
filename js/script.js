ef96806d095707f2d8065f2eb26a53011640376a
Marijoke <mariam.adamu@hotmail.co.uk>
2021-03-13 14:22:05 +0000

    Completed matching functions. Used Ternary Operator to clean up code

--- js/script.js
+++ js/script.js
@@ -8,21 +8,30 @@ function flipCard(){
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
-    } else {
+        return
+    } 
         hasFlippedCard = false;
         secondCard = this;
         
-        if (firstCard.dataset.name === secondCard.dataset.name) {
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        } else {
-            setTimeout(() => {
-            firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip');
-            }, 1500)
-        }
-
-        console.log("Function was executed!");
+       checkForMatch();
     }
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+     
+    isMatch ? disableCards() : unflipCards();
 }
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards(){
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
+}
+
 cards.forEach(card => card.addEventListener('click', flipCard));
