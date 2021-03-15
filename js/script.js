cd261b7fe09fd98575144e83958c134f5f997c5e
Marijoke <mariam.adamu@hotmail.co.uk>
2021-03-15 22:46:11 +0000

    Updated the Level 1 and Level 2 page. Added new pictures in line with new brand direction. cleaned up some javasript code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
+// gets the memory card selection
+
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let counter = 0;
+
 
 function flipCard(){
     if (lockBoard) return;
@@ -17,16 +21,30 @@ function flipCard(){
         
        checkForMatch();
     }
+
 function checkForMatch() {
     let isMatch = firstCard.dataset.name === secondCard.dataset.name;
-     
     isMatch ? disableCards() : unflipCards();
+    if (isMatch) {
+        counter ++;
+        console.log("isMatch: ", counter);
+        if (counter === 6) {
+            console.log("yay, you've won!");
+            window.location = "level2.html";
+        }
+        disableCards();
+    } else {
+        console.log("notMatch: ",counter);
+    }
+     // we need to find out here if all cards have been flipped or if no card is left for flipping
 }
+
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
     resetBoard();
 }
+
 function unflipCards(){
     lockBoard = true;
     setTimeout(() => {
@@ -35,6 +53,7 @@ function unflipCards(){
         resetBoard();
     }, 1500);
 }
+
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
@@ -42,17 +61,17 @@ function resetBoard() {
 
 (function shuffle() {
   cards.forEach(card => {
-    let ramdomPos = Math.floor(Math.random() * 12);
-    card.style.order = ramdomPos;
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
   });
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
+  let dots = document.getElementById("dots");
+  let moreText = document.getElementById("more");
+  let btnText = document.getElementById("myBtn");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
